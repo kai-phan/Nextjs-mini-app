e3f2ebac24981370f8070543c2bfe76adf4f5f62
tinnguyen <tinnt.developer@gmail.com>
2023-07-28 22:35:12 +0700

    Add container

---
+++ src/containers/subdomain1/CodeContainer/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export type Props = {
+  className?: string;
+};
+
+const CodeContainer: React.FC<Props> = () => {
+  return (
+    <React.Fragment>
+      <div>CodeContainer CODE CODE CODE</div>
+    </React.Fragment>
+  );
+};
+
+export default CodeContainer;
---
+++ src/pages/subdomain1/[code].tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import CodeContainer from 'src/containers/subdomain1/CodeContainer';
+
+export default function DetailPage({ code }) {
+  return (
+    <div>
+      <CodeContainer />
+    </div>
+  );
+}
+
+export async function getServerSideProps({ locale }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale)),
+    },
+  };
+}
