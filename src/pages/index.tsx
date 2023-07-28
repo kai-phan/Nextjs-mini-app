49b251063ce35a8b6d511cfa45c55886d12d7e9d
Quy Phan <quyphan@Quys-MacBook-Pro-2.local>
2023-07-28 20:51:14 +0700

    add example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,24 @@ import React from 'react';
 import { GetServerSidePropsContext } from 'next';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import { Button } from 'antd';
 
 export default function Home() {
   const { t } = useTranslation();
+  const router = useRouter();
+
+  const changeTo = router.locale === 'en' ? 'vi' : 'en';
 
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
-      <Button type="primary">{t('test')}</Button>
+      <Link href="/" locale={changeTo}>
+        <Button>{t('test', { changeTo })}</Button>
+      </Link>
     </main>
   );
 }
